7fcdaed2851d5035c8e5a032b7f0057ac59e94cf
hank <hank-c@qq.com>
2024-06-22 11:23:49 +0800

    feat: storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   base: "",
   build: {
     outDir: "docs",
-  }
+  },
+  test: {
+    environment: "jsdom",
+  },
 })
-    
